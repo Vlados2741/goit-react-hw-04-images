30ef38a22f329c3a28e3154d2954a7ac9d160911
Vlados2741 <ramosinho3@gmail.com>
2022-11-30 13:04:09 +0200

    new api

--- src/service/api.js
+++ src/service/api.js
@@ -1,18 +1,9 @@
-import axios from 'axios';
-
-// const URL = 'https://pixabay.com/api/';
 const KEY = '29359715-57cbbaa05904a72f5703b5006';
-const PARAMETERS = 'image_type=photo&orientation=horizontal&per_page=12';
-axios.defaults.baseUrl = 'https://pixabay.com/api/';
-axios.defaults.params = {
-  key: KEY,
-  image_type: photo,
-  orientation: horizontal,
-  per_page: 12,
-};
+
 export const api = async (inputValue, page) => {
-  const response = await axios.get(
-    `${baseUrl}?q=${inputValue}&page=${page}&${axios.defaults.params.key}&${axios.defaults.params.image_type}&${axios.defaults.params.orientation}&${axios.defaults.params.per_page}`
+  const response = await fetch(
+    `https://pixabay.com/api/?key=${KEY}&q=${inputValue}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
   );
-  return response.data;
+  const images = await response.json();
+  return images;
 };
